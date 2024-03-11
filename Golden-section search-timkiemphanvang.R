c86480459ef28ddd8471498e79581f70f89b731e
duyavaliable <129377700+duyavaliable@users.noreply.github.com>
2024-03-11 15:21:51 +0700

    Update Golden-section search-timkiemphanvang.R

--- Golden-section search-timkiemphanvang.R
+++ Golden-section search-timkiemphanvang.R
@@ -1,5 +1,5 @@
 goldensection = function(f, a,b, tol){
-  gr = (sqrt(5) + 1)/2
+  gr = (sqrt(5) - 1)/2
   
   d = gr*(b-a)
   x1 = a + d
@@ -31,4 +31,4 @@ goldensection = function(f, a,b, tol){
 f = function(x) {x*x - 6*x + 15}
 result = goldensection(f, a= 0, b = 6, tol = 1e-16)
 print(result)
-#chua xong code van chua hoan thien van xay ra loi
+
